eb261341757403121f5e0c361d9f57a7fa0441c2
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2023-09-15 14:17:15 -0300

    Create Project and modeling database

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { invoke } from "@tauri-apps/api/tauri";
+
+const a = () => {
+  invoke("my_custom_command");
+};
+</script>
+
+<template>
+  <button @click="() => a()"></button>
+</template>

8a50ee2e9e95243edcc41fefaf87b4358a5ae9c8
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2023-09-15 15:23:53 -0300

    Installing vue-router and configure him

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import { primeVuePlugin } from "./plugins/prime-vue";
+import { router } from "./routes";
 
 import App from "./App.vue";
 
 import "./styles.scss";
 
-createApp(App).use(primeVuePlugin).mount("#app");
+createApp(App).use(primeVuePlugin).use(router).mount("#app");

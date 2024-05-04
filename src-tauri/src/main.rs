63a4d2ce2e0933d0306d3750d3538b25118a8d1d
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2024-05-04 11:46:02 -0300

    Remove handler functions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,5 @@
-mod manage_files;
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            manage_files::list_all_saves,
-            manage_files::delete_save
-        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

572a42da2ea637906c6854c42db16441555e9d4d
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2024-05-07 15:20:16 -0300

    [Off Topic] New file button logic implemented

--- src/pages/SelectPerfilPage.vue
+++ src/pages/SelectPerfilPage.vue
@@ -8,6 +8,7 @@ import {
   FileEntry,
 } from "@tauri-apps/api/fs";
 import { onBeforeMount, ref } from "vue";
+import { database } from "../api";
 
 const loadSavesFolder = async () => {
   const savesList = await readDir("game_schedule", {
@@ -39,13 +40,20 @@ const showWarningDeleteSaveMenu = () => {
 const saveName = ref<string>("");
 
 const saves = ref<FileEntry[]>();
+
+const newFileName = ref<string>("");
+const createNewFile = async () => {
+  if (newFileName.value) {
+    await database.createNewFile(newFileName.value);
+  }
+};
 </script>
 <template>
   <div class="flex justify-content-center mt-6">
     <div class="w-6 gap-3 flex flex-column">
       <div class="flex w-full gap-3">
         <PvInputText class="flex-1" />
-        <PvButton label="Novo Perfil" />
+        <PvButton label="Novo Perfil" @click="createNewFile()" />
         <PvButton icon="pi pi-undo" @click="() => loadSavesFolder()" />
       </div>
       <PvDataTable :value="saves" paginator :rows="5" class="w-full">

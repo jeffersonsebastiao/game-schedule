e5c2d05f49f7f0eff2f41f0c42eff46f707580cc
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2024-05-08 13:09:54 -0300

    Change constructor param type

--- src/api/console/ConsoleRepository.ts
+++ src/api/console/ConsoleRepository.ts
@@ -2,35 +2,31 @@ import { Database } from "../database";
 import { IConsole } from "./interfaces";
 
 export class ConsoleRepository {
-  constructor(private readonly database: Database) {}
+  constructor(private readonly query: Database["query"]) {}
 
   async create(data: Omit<IConsole, "id">) {
-    return await this.database.query("INSERT INTO consoles VALUES (?1, ?2)", [
+    return await this.query("INSERT INTO consoles VALUES (?1, ?2)", [
       null,
       data.name,
     ]);
   }
 
   async update(data: IConsole) {
-    return await this.database.query(
-      "UPDATE consoles SET name = ?1 WHERE ID = ?2",
-      [data.name, data.id]
-    );
+    return await this.query("UPDATE consoles SET name = ?1 WHERE ID = ?2", [
+      data.name,
+      data.id,
+    ]);
   }
 
   async getOne(data: Omit<IConsole, "name">) {
-    return await this.database.query("SELECT * FROM consoles WHERE id = ?", [
-      data.id,
-    ]);
+    return await this.query("SELECT * FROM consoles WHERE id = ?", [data.id]);
   }
 
   async getAll() {
-    return await this.database.query("SELECT * FROM consoles");
+    return await this.query("SELECT * FROM consoles");
   }
 
   async delete(data: Omit<IConsole, "name">) {
-    return await this.database.query("DELETE FROM consoles WHERE id = ?", [
-      data.id,
-    ]);
+    return await this.query("DELETE FROM consoles WHERE id = ?", [data.id]);
   }
 }

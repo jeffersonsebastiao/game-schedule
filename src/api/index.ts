9de096eed9112aa85b94928b48ae4e93dc59b411
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2024-05-08 19:03:30 -0300

    Database methods injected on genderRepository instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,13 @@
 import { ConsoleRepository } from "./console/ConsoleRepository";
 import { Database } from "./database";
+import { GenderRepository } from "./gender/GenderRepository";
 
 export const database = new Database();
 export const consoleRepository = new ConsoleRepository(
+  database.query.bind(database),
+  database.select.bind(database)
+);
+export const genderRepository = new GenderRepository(
   database.query.bind(database),
   database.select.bind(database)
 );

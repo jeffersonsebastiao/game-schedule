9ecb23e79b53cbeae7be85cd904b54076a395b52
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2024-05-08 18:48:01 -0300

    Inject select method from Database class in consoleRepository instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,5 +3,6 @@ import { Database } from "./database";
 
 export const database = new Database();
 export const consoleRepository = new ConsoleRepository(
-  database.query.bind(database)
+  database.query.bind(database),
+  database.select.bind(database)
 );

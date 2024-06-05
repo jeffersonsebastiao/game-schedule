f6cdfc351dac41b535fbd41f2991b85ecffdec94
jeffersonsebastiao <jefferson_gutz@hotmail.com>
2024-06-05 19:52:15 -0300

    Route to main page created

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import SelectPerfilPage from "../pages/SelectPerfilPage.vue";
 import ConsolePage from "../pages/ConsolePage.vue";
 import MainLayout from "../layout/MainLayout.vue";
 import TypePage from "../pages/TypePage.vue";
+import MainPage from "../pages/MainPage.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -15,6 +16,10 @@ export const router = createRouter({
       path: "/",
       component: MainLayout,
       children: [
+        {
+          path: "main",
+          component: MainPage,
+        },
         {
           path: "console",
           component: ConsolePage,
